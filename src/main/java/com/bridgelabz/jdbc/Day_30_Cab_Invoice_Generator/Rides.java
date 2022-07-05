d898b09e4b2ce6bc4bd1c3f6d17264a5c499e164
srajashekhar <udayarajashekhar@gmail.com>
2022-07-05 08:38:23 +0530

    Added: Tested Multiple Rides

---
+++ src/main/java/com/bridgelabz/jdbc/Day_30_Cab_Invoice_Generator/Rides.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.jdbc.Day_30_Cab_Invoice_Generator;
+
+public class Rides {
+
+	public double distance;
+	public int time;
+
+	public Rides(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+
+}
--- src/test/java/com/bridgelabz/jdbc/Day_30_Cab_Invoice_Generator/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/jdbc/Day_30_Cab_Invoice_Generator/CabInvoiceGeneratorTest.java
@@ -8,15 +8,24 @@ public class CabInvoiceGeneratorTest {
 	@Test
 	public void givenDistanceAndTime_ShouldReturnTotalFare() {
 		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-		double TotalFare = cabInvoiceGenerator.CalculateFare(5.0, 4);
-		Assert.assertEquals(54, TotalFare, TotalFare);
+		double totalFare = cabInvoiceGenerator.CalculateFare(5.0, 4);
+		Assert.assertEquals(54, totalFare, totalFare);
 	}
 
 	@Test
 	public void givenDistanceAndTime_ShouldReturnMinFare() {
 		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-		double TotalFare = cabInvoiceGenerator.CalculateFare(0.1, 2);
-		Assert.assertEquals(5, TotalFare, TotalFare);
+		double totalFare = cabInvoiceGenerator.CalculateFare(0.1, 2);
+		Assert.assertEquals(5, totalFare, totalFare);
 	}
 
+	@Test
+	public void givenMultipleRides_ShouldCalculateAggregateTotal() {
+
+		Rides[] rides = { new Rides(5, 4), new Rides(0.1, 2) };
+
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double totalFare = cabInvoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(54, totalFare, totalFare);
+	}
 }
